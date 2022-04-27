f90d0099a1e2eaf3321a81f1c4561b5772eaaaac
Kind24 <bonnevieugolinitheo@gmail.com>
2022-04-27 19:27:10 +0200

    Ajout login

---
+++ RouletteConsole.py
@@ -0,0 +1,82 @@
+import random
+import csv
+
+# Fonctions 
+def listeLogin(liste) :
+    a = []
+    for i in range(1,len(liste)) :
+        l = liste[i][1]
+        a.append(l)
+    return a
+
+def choixLigne(user) :
+    for i in range(len(allLogin)) :
+        if user == allLogin[i] :
+            return i
+
+# Définition Marqueurs 
+Cursor = 0
+
+# Définition Gains
+zero = 0
+rouge = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
+noir = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
+pair = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
+impair = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+manque = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+passe = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+
+# Définition fichier utilisateur
+userFile = open("userRoulette.csv")
+userList = list(csv.reader(userFile, delimiter=";"))
+allLogin = listeLogin(userList)
+
+# Bienvenue
+print("Bienvenue à la Roulette Magique !")
+choixConnexion = int(input("Voulez-vous vous Connecter(1) ou vous Enregistrer(2) : "))
+if choixConnexion == 1:
+    while Cursor == 0:
+        login = input("Votre pseudo : ")
+        if login in allLogin :
+            Cursor = 1
+        else : 
+            print("Pseudo inconnu !")
+    Cursor = 0
+    ligneUser = choixLigne(login)+1
+    while Cursor == 0:
+        password = input("Votre mot de passe : ")
+        if password == userList[ligneUser][2]:
+            Cursor = 1
+        else :
+            print("Mot de passe incorrect !")
+    Cursor = 0
+    print("Bienvenue ,", login, "!")
+elif choixConnexion == 2:
+    while Cursor == 0:
+        nouveauMail = input("Entrez votre eMail : ")
+        if "@" in nouveauMail and "." in nouveauMail :
+            Cursor = 1
+        else :
+            print("Format eMail incorrect !")
+    Cursor = 0
+    while Cursor == 0:
+        nouveauPseudo = input("Entrez un pseudo : ")
+        if nouveauPseudo in allLogin :
+            print("Pseudo déjà pris !")
+        else :
+            Cursor = 1
+    Cursor = 0
+    while Cursor == 0:
+        nouveauPass1 = input("Entrez un nouveau mot de passe (6 caractères minimum) : ")
+        if len(nouveauPass1) >=6 :
+            Cursor = 1
+        else:
+            print("Trop peu de caractères")
+    Cursor = 0
+    while Cursor == 0:
+        nouveauPass2 = input("Entrez à nouveau votre mot de passe : ")
+        if nouveauPass2 == nouveauPass1:
+            Cursor = 1
+        else:
+            print("Mot de passe incorrect !")
+    Cursor = 0
